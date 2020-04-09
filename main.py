b342d55704fdfbe722a1aa735193784a270be986
VictorPimentel06 <victor.pimentel@runahr.com>
2020-04-09 00:23:28 -0500

    pagination

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ class Zendesk_support():
         Extraccion de tickets desde Zendesk Support.
         """
         self.incremental = "https://runahr.zendesk.com/api/v2/incremental/tickets.json?start_time="
-        self.session = requests.Session()
 
     def Tickets(self, fecha = None, tipo = "complete"): 
         """
@@ -24,22 +23,34 @@ class Zendesk_support():
         """
         tickets = []
         if tipo == "complete": 
-            fecha = int(datetime.datetime.strptime("2018-01-01","%Y-%M-%d").timestamp())
-            response = self.session.get(self.incremental + str(fecha), auth = (os.environ["ZENDESK_USER"], os.environ["ZENDESK_PASSWORD"])).json()
+            fecha = int(datetime.datetime.strftime("2018-01-01","%Y-%m-%d").timestamp())
+            response = requests.get(self.incremental + str(fecha), auth = (os.environ["ZENDESK_USER"], os.environ["ZENDESK_PASSWORD"]))
+            data = response.json()
             tickets.extend(response['tickets'])
         if tipo == "partial": 
-            fecha = int(datetime.datetime.strptime(fecha, "%Y-%M-%d").timestamp())
-            response = self.session.get(self.incremental + str(fecha), auth = (os.environ["ZENDESK_USER"], os.environ["ZENDESK_PASSWORD"])).json()
-
-        while response['next_page']: 
-            tickets.extend(response['tickets'])
-            response = self.session.get(response['next_page'], auth = (os.environ["ZENDESK_USER"], os.environ["ZENDESK_PASSWORD"])).json()
-            print(response['next_page'])
+            fecha = int(datetime.datetime.strptime(fecha, "%Y-%m-%d").timestamp())
+            response = requests.get(self.incremental + str(fecha), auth = (os.environ["ZENDESK_USER"], os.environ["ZENDESK_PASSWORD"]))
+            if response.status_code != 200: 
+                print("Error en la extraccion. CodeError: "+ str(response.status_code))
+            data = response.json()
+            url = data['next_page']
+            tickets.extend(data['tickets'])
+        while url: 
+            response = requests.get(url, auth = (os.environ["ZENDESK_USER"], os.environ["ZENDESK_PASSWORD"]))
+            if response.status_code != 200: 
+                print("Error en la extraccion. CodeError: "+ str(response.status_code))
+            data = response.json()
+            tickets.extend(data['tickets'])
+            if url == data['next_page']:
+                break
+            url = data["next_page"]
+            print(len(tickets))
             
             
+            
 
 
 
 
 
-Zendesk_support().Tickets(fecha = "2020-04-01",tipo = "partial")
+Zendesk_support().Tickets(fecha = "2020-01-01",tipo = "partial")
